9181a5ae7c52d3c38f5b071faed499427c79ee89
ThisDudeBoy <alex.moreira74@outlook.fr>
2021-04-15 03:58:19 +0200

    Now supporting gif avatar !

--- commands/informations/avatar.js
+++ commands/informations/avatar.js
@@ -15,7 +15,7 @@ module.exports = {
         embed.setColor(client.config.embed.color);
         embed.setDescription('Avatar of **' + user.username + '** ');
         embed.setAuthor(message.author.username, message.author.avatarURL({ dynamic: true }));
-        embed.setImage(user.avatarURL())
+        embed.setImage(user.avatarURL({ dynamic: true }))
         embed.setTimestamp();
         embed.setFooter(client.config.embed.footer);
 
