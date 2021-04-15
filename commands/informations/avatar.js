91c435c4b685476672f0105e5b4a9143eb5c59eb
ThisDudeBoy <alex.moreira74@outlook.fr>
2021-04-15 04:11:30 +0200

    Removing useless code

--- commands/informations/avatar.js
+++ commands/informations/avatar.js
@@ -6,9 +6,6 @@ module.exports = {
     category: 'informations',
 
     execute(client, message) {
-        const categories = [];
-        client.commands.forEach((command) => categories.includes(command.category) ? false : categories.push(command.category));
-
         const user = message.mentions.users.first() || message.author;
         const embed = new MessageEmbed();
 
--- commands/informations/getBot.js
+++ commands/informations/getBot.js
@@ -6,11 +6,9 @@ module.exports = {
     category: 'informations',
 
     execute(client, message) {
-        const categories = [];
-        client.commands.forEach((command) => categories.includes(command.category) ? false : categories.push(command.category));
 
         const embed = new MessageEmbed();
-
+        
         embed.setColor(client.config.embed.color);
         embed.setTitle('Invite Me !');
         embed.setThumbnail(client.user.avatarURL());
