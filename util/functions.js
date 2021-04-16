e46e718aa45b7dd179c41adefafa53d0774239f9
WaDixix <romaingau18000@gmail.com>
2021-04-16 03:53:29 +0200

    Add clean function

--- util/functions.js
+++ util/functions.js
@@ -2,16 +2,20 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 
-    // Generate random number between min and max
+	// Generate random number between min and max
 	randomNumber(min, max) {
 		return Math.floor(Math.random() * (max - min)) + min;
 	},
 
 	sendEmbedError(client, message) {
 		const embed = new MessageEmbed();
-        embed.setColor(client.config.embed.color);
-        embed.setDescription("❌ | " + message);
+		embed.setColor(client.config.embed.color);
+		embed.setDescription("❌ | " + message);
 		return embed;
 	},
+
+	clean(text) {
+		return (typeof(text) === "string") ? text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203)) : text;
+	}
 
 };
